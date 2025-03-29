55741d294f4513ce9646d6513b023fe037e95964
Song Lei <songlei198974@gmail.com>
2025-03-29 08:00:50 +0800

    s

--- ThermoProcess/src/Utility/DocumentDB.java
+++ ThermoProcess/src/Utility/DocumentDB.java
@@ -8,11 +8,21 @@ import java.util.Map;
 public class DocumentDB {
 	
 	private static final Map<String,List<String>> SOFTWARE_DOCS = new HashMap<>();
+	public static final String PHASE_ONE_CONCEPT = "Concept(DTI-DTC)";
 	
 	static {
-		SOFTWARE_DOCS.put("Concept(DTI-DTC)", Arrays.asList("Customer Wants","CRD", "CRD Review(checkpoint|dev)", "Tech Assessment(dev)", "Tech Assessment Review(checkpoint|dev)"));
+		SOFTWARE_DOCS.put(PHASE_ONE_CONCEPT, Arrays.asList("Customer Wants","CRD", "CRD Review(Checkpoint|dev)", "Tech Assessment(dev)", "Tech Assessment Review(Checkpoint|dev)"));
 		SOFTWARE_DOCS.put("Definition(DTC-SDC)", null);
 		SOFTWARE_DOCS.put("Development(SDC-SMC)", null);
 		SOFTWARE_DOCS.put("Manufacture(SMC-CTC)", null);
+	}
+	
+	
+	public static void print(String phase) {
+		String blue = "\u001B[34m";
+		String red = "\u001B[31m";
+		String reset = "\u001B[0m";
+		System.out.println(red + phase + ":" + reset);
+		System.out.println(blue + SOFTWARE_DOCS.get(phase) + reset);
 	}
 }
--- ThermoProcess/src/documents/Ask.java
+++ ThermoProcess/src/documents/Ask.java
@@ -1,8 +1,10 @@
 package documents;
 
+import Utility.DocumentDB;
+
 public class Ask {
 
 	public static void main(String[] args) {
-		
+		DocumentDB.print(DocumentDB.PHASE_ONE_CONCEPT);
 	}
 }
